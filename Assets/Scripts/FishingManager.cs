6292905e81f7a2ca541b2faa0f2b5cda3b0bf121
tjwjdgus12 <01048733438a@gmail.com>
2021-11-22 14:03:01 +0900

    Add FishingManager

---
+++ Assets/Scripts/FishingManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingManager : MonoBehaviour
+{
+    public FishingManager()
+    {
+        
+    }
+
+    public void StartFishing()
+    {
+        // 낚시 미니게임 시작
+
+        // GameManager 정보를 바탕으로 물고기 후보 추출
+        // 희귀도를 고려해서 미끼를 물 물고기 결정
+        // 게임
+    }
+}
