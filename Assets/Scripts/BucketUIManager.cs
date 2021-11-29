bb6a25a91de50935c6e2ddf72ad3f7e0b67569c2
hmkim199 <hmkim199@gmail.com>
2021-11-29 15:57:35 +0900

    feat: working on bucket UI button click event

--- Assets/Scripts/BucketUIManager.cs
+++ Assets/Scripts/BucketUIManager.cs
@@ -1,22 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class BucketUIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject fishScrollView;
+    [SerializeField] GameObject baitPanel;
+    [SerializeField] GameObject fishBtn;
+    [SerializeField] GameObject baitBtn;
+
+    Color clickedColor = Color.white;
+    Color defaultColor = new Color(214, 214, 214);
+
+/*    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
+    }*/
 
     public void OnClickInventory() {
         gameObject.SetActive(true);
+        Button fishTab = fishBtn.GetComponent<Button>();
     }
+
+    public void OnClickFishTab() {
+        baitPanel.SetActive(false);
+        fishScrollView.SetActive(true);
+
+        // 현재 내 물고기 인벤토리 보여주기 (이미지 크기도 조절)
+        
+    }
+
+    public void OnClickBaitTab() {
+        fishScrollView.SetActive(false);
+        baitPanel.SetActive(true);
+
+        // 현재 내 미끼 정보 보여주기. 갯수 바꾸기.
+    }
+
+    public void OnClickExitBtn() {
+        GameObject clickedObject = EventSystem.current.currentSelectedGameObject;
+        clickedObject.transform.parent.gameObject.SetActive(false);
+        baitPanel.SetActive(false);
+        fishScrollView.SetActive(false);
+    }
+
+/*    private void ChangeBtnNormalColor(Button button, Color color) {
+        ColorBlock cb = button.colors;
+        cb.normalColor = color;
+        button.colors = cb;
+    }*/
 }
