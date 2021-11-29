75ee074c9d3cd210cc411ed93946df4ceae1f550
hmkim199 <hmkim199@gmail.com>
2021-11-29 18:30:55 +0900

    feat: working on bucket script

--- Assets/Scripts/BucketUIManager.cs
+++ Assets/Scripts/BucketUIManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class BucketUIManager : MonoBehaviour
 {
@@ -10,6 +12,9 @@ public class BucketUIManager : MonoBehaviour
     [SerializeField] GameObject baitPanel;
     [SerializeField] GameObject fishBtn;
     [SerializeField] GameObject baitBtn;
+    [SerializeField] GameObject fishPrefab;
+
+    private GameObject[] fishCards;
 
     Color clickedColor = Color.white;
     Color defaultColor = new Color(214, 214, 214);
@@ -36,7 +41,38 @@ public class BucketUIManager : MonoBehaviour
         fishScrollView.SetActive(true);
 
         // 현재 내 물고기 인벤토리 보여주기 (이미지 크기도 조절)
+        int fishCnt = 6;
+        try
+        {
+            fishCnt = Player.Instance.FishTank.Count;
+        }
+        catch(NullReferenceException e) {
+            print(e);
+        }
         
+        fishCards = new GameObject[fishCnt];
+        for (int i = 0; i < fishCnt; i++)
+        {
+            fishCards[i] = Instantiate(fishPrefab);
+            Transform fishTransform = fishCards[i].transform;
+            fishTransform.parent = fishScrollView.transform.GetChild(0).GetChild(0); // scrollview 자식인 viewport의 자식인 content를 부모로 설정.
+            fishTransform.localScale = Vector3.one;
+
+            try
+            {
+                Transform fishName = fishTransform.GetChild(0);
+                Transform fishImg = fishTransform.GetChild(1);
+                fishName.GetComponent<TextMeshProUGUI>().text = Player.Instance.FishTank[i].Name;
+                fishImg.GetComponent<Image>().sprite = Resources.Load<Sprite>("FishImg") as Sprite;
+                fishImg.GetComponent<Image>().color = Color.red;
+                float size = Player.Instance.FishTank[i].Size;
+                fishImg.GetComponent<RectTransform>().sizeDelta = new Vector2(size, size);
+            }
+            catch (NullReferenceException e)
+            {
+                print(e);
+            }
+        }
     }
 
     public void OnClickBaitTab() {
