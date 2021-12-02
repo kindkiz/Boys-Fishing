b1582109e363f971b39bf351e75294eb1c5c8dec
hmkim199 <hmkim199@gmail.com>
2021-12-02 17:05:22 +0900

    feat: add audio source, working on about audio

--- Assets/Scripts/SettingUIManager.cs
+++ Assets/Scripts/SettingUIManager.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class SettingUIManager : MonoBehaviour
 {
-/*    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /*    // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }*/
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }*/
+
+    public void ChangeAudioSetting(bool onAudio) {
+        gameObject.GetComponentInParent<AudioSource>().enabled = onAudio;
+    }
 
     public void OnClickSettingBtn() {
         gameObject.SetActive(true);
