347f0e4b9a3e7cf67e2bea03e24555a68beca157
hmkim199 <hmkim199@gmail.com>
2021-12-02 13:47:24 +0900

    feat: show user info in UI

--- Assets/Scripts/UserInfoUIManager.cs
+++ Assets/Scripts/UserInfoUIManager.cs
@@ -2,18 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using System;
 
 public class UserInfoUIManager : MonoBehaviour
 {
-    [SerializeField] GameObject boy;
-    [SerializeField] GameObject ship;
-    [SerializeField] GameObject rod;
-    [SerializeField] GameObject line;
-    [SerializeField] GameObject reel;
+    [SerializeField] Transform infosParent;
+    Transform[] infos;
+
+    private void Awake()
+    {
+        int childCnt = infosParent.childCount;
+        infos = new Transform[childCnt];
+        for (int i = 0; i < childCnt; i++)
+        {
+            infos[i] = infosParent.GetChild(i);
+        }  
+    }
 
     public void OnClickUserInfo() {
-        ChangeUserInfo();
         gameObject.SetActive(true);
+        ChangeUserInfo();
     }
 
     public void OnClickExitBtn() {
@@ -21,9 +30,22 @@ public class UserInfoUIManager : MonoBehaviour
     }
 
     private void ChangeUserInfo() {
-        // TODO: boy -> level 바꾸기
-        // boy.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = Player.Instance.GetAverageLevel();
+        // rod, reel, line, ship -> img, level, name 바꾸기
+        for (int i = 0; i < infos.Length - 1; i++) {
+            Sprite img = Player.Instance.Equip[(Etype)i].EqSprite;
+            string level = Player.Instance.Equip[(Etype)i].Level.ToString();
+            string name = Player.Instance.Equip[(Etype)i].Name;
+            ChangeInfo(infos[i], img, level, name);
+        }
 
-        // TODO: ship, rod, line, reel -> img, level, name 바꾸기
+        // boy -> level 바꾸기
+        infos[infos.Length - 1].GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = Player.Instance.GetAverageLevel().ToString();
+              
+    }
+
+    private void ChangeInfo(Transform t, Sprite img, string level, string name) {
+        t.GetComponent<Image>().sprite = img;
+        t.GetChild(0).GetComponent<TextMeshProUGUI>().text = level;
+        t.GetChild(1).GetComponent<TextMeshProUGUI>().text = name;
     }
 }
