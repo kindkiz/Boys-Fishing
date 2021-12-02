c986af8c24cf3d766b7429eb37e7a78d848733d5
hmkim199 <hmkim199@gmail.com>
2021-12-02 10:44:05 +0900

    feat: add setting box UI

--- Assets/Scripts/UserInfoUIManager.cs
+++ Assets/Scripts/UserInfoUIManager.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UserInfoUIManager : MonoBehaviour
 {
     [SerializeField] GameObject boy;
-    [SerializeField] GameObject boat;
+    [SerializeField] GameObject ship;
     [SerializeField] GameObject rod;
     [SerializeField] GameObject line;
     [SerializeField] GameObject reel;
 
     public void OnClickUserInfo() {
+        ChangeUserInfo();
         gameObject.SetActive(true);
     }
 
@@ -18,8 +20,10 @@ public class UserInfoUIManager : MonoBehaviour
         gameObject.SetActive(false);
     }
 
-    private void ChangeUserInfo() { 
+    private void ChangeUserInfo() {
         // TODO: boy -> level 바꾸기
-        // TODO: boat, rod, line, reel -> img, level, name 바꾸기
+        // boy.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = Player.Instance.GetAverageLevel();
+
+        // TODO: ship, rod, line, reel -> img, level, name 바꾸기
     }
 }
